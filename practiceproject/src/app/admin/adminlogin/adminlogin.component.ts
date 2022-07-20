b6a0bcaba499e86dea82368f280cac565dcf4f4b
santoshmulak <santoshmulak98@gmail.com>
2022-07-20 10:40:59 +0530

    button click

--- practiceproject/src/app/admin/adminlogin/adminlogin.component.ts
+++ practiceproject/src/app/admin/adminlogin/adminlogin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminlogin',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminloginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
+
+  routing(){
+  // this.router.navigateByUrl('admin/adminLogout')
+  this.router.navigateByUrl('login') 
+}
 
 }
